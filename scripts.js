2857bfcc09229961549b1af544a879073a0e8da5
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-08-13 14:29:09 +0100

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+"use strict";
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert("Thank you for signing up! We will contact you shortly.");
+});
